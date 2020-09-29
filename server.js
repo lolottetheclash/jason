40fcc34eba1afba89db0945558414e27f16f8af3
lauren <lauren@MacBook-Pro-de-user.local>
2020-09-29 10:45:03 +0200

    Rename var

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config({ path: './config/config.env' });
 
 // Connect to db
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
