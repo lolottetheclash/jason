d539c1d02caf7b19604818ed28369fe106038f37
lauren Olivier <35332974+lolottetheclash@users.noreply.github.com>
2021-01-09 12:31:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server listening on Port ${PORT}, ${process.env.NODE_ENV}`)
+  console.log(`Server listening on Port ${PORT}.`)
 );
